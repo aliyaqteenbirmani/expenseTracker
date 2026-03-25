0b3a1bf09ce094b864451cd03e6ea61b30c5bcb5
Ali Yaqteen <aliyaqteenbirmani512@gmail.com>
2026-03-25 11:37:24 +0500

    added git ignore

--- ExpenseTrackingSystem/obj/Debug/net9.0/ExpenseTrackingSystem.API.AssemblyInfo.cs
+++ ExpenseTrackingSystem/obj/Debug/net9.0/ExpenseTrackingSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackingSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47eb37ecf980bc1c6d2e4eef013db9027a9c38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackingSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackingSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
